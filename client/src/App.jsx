98687aee6ccf01a0ccad466887252061b0927da9
harshdev2909 <harsh.dev2909@gmail.com>
2024-10-02 20:51:56 +0530

    added

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button>subscribe</Button>
+     
+    </div>
+  )
+}
+
+export default App
