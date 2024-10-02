934ac3e81520ec653e2ccccd9330cf2a33022d77
harshdev2909 <harsh.dev2909@gmail.com>
2024-10-02 21:00:36 +0530

    Routing Done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,21 @@ import { StrictMode,react } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Index from './create-trip/Index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },{
+    path:'/create-trip',
+    element:<Index/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
